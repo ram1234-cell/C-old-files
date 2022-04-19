0071c9e00757e592b828b2a680962bfe373e7db1
S D Sriram <sdsriram1@gmail.com>
2022-04-19 13:36:45 +0530

    -

--- Staements.c
+++ Staements.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main()
@@ -9,16 +8,4 @@ int main()
 		printf("numa is greater\n");
 	}
 	return 0;
-=======
-#include <stdio.h>
-
-int main()
-{
-	int num = 0;
-	int numa = 34;
-	if (num<numa){
-		printf("numa is greater\n");
-	}
-	return 0;
->>>>>>> cf22a45c32f00113ae0bbd65ed3297249061482c
 }
---
+++ statement_and_fuction.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function 1
+void my_funtion_one(){
+    printf("a is smaller/n");
+}
+
+//Function 2
+
+void my_function_two(){
+
+    printf("b is bigger\n");
+}
+
+
+int main(){
+    int a = 1;
+    int b = 2;
+    if (a>b){
+        my_funtion_one();
+        
+    return 0;
+    }
+        
+    
+
+}
